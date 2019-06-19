14c31e5e2106c4fc8cc01327df3598c9bc8f956e
shukor <sabdrazak@gmail.com>
2019-06-19 16:06:02 +0800

    update title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Selamat Hari Raya
                 </div>
 
                 <div class="links">
